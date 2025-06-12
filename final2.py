9203bdcf2149d23802a6de3f095d32b17ff35c03
Rawan69 <128935411+Rawan69@users.noreply.github.com>
2025-06-13 01:26:06 +0300

    Update final2.py

--- final2.py
+++ final2.py
@@ -160,5 +160,5 @@ def predict():
 
     return jsonify({"error": "No plant detected", "plant_predictions": [], "disease": "unknown"}), 200
 
-if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
